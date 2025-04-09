a24dee31cd749b52a95e5b1df3cfaa0eef5c8dc8
wubin <2538201831@qq.com>
2025-04-09 08:56:20 +0800

    fix 修改咨询师拼写及GetRunningSession接口返回值

--- src/main/java/org/example/psychologicalcounseling/constant/OtherConstant.java
+++ src/main/java/org/example/psychologicalcounseling/constant/OtherConstant.java
@@ -2,5 +2,5 @@ package org.example.psychologicalcounseling.constant;
 
 public class OtherConstant {
     static final public Long SessionTimeout = 600000L;
-    static final public Long RequestTimeout = 600000000L;
+    static final public Long RequestTimeout = 600000L;
 }
