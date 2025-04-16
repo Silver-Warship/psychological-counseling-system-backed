18b8f3b38a93dc5cc14b314442063672388427f2
Chen-Z-hub <2574680556@qq.com>
2025-04-16 08:59:49 +0800

    modified:   src/main/java/org/example/psychologicalcounseling/controller/UserInfoController.java

--- src/main/java/org/example/psychologicalcounseling/module/user/info/UserInfoService.java
+++ src/main/java/org/example/psychologicalcounseling/module/user/info/UserInfoService.java
@@ -2,6 +2,7 @@ package org.example.psychologicalcounseling.module.user.info;
 
 import org.example.psychologicalcounseling.dto.UserWithSessionsDto;
 import org.example.psychologicalcounseling.model.Account;
+import org.example.psychologicalcounseling.model.Admin;
 import org.example.psychologicalcounseling.model.Session;
 import org.example.psychologicalcounseling.model.User;
 import org.example.psychologicalcounseling.repository.AccountRepository;
@@ -60,6 +61,16 @@ public class UserInfoService {
         return user.getNickname();
     }
 
+    public User.Gender getGenderByEmail(String email) {
+        // 从数据库中获取用户信息，并检查用户是否存在
+        User user = userRepository.findByEmail(email);
+        if (user == null) {
+            // 用户不存在，返回-1或其他错误处理
+            return null;
+        }
+        return user.getGender();
+    }
+
     public boolean editUserProfile (EditRequestDto editRequestDto){
         try{
             Long uid = editRequestDto.getUid();
@@ -68,13 +79,13 @@ public class UserInfoService {
             Account account = accountRepository.findById(uid).orElseThrow(() -> new RuntimeException("Account not found"));
 
             // 更新不为空的字段
-            if (editRequestDto.getGender() != null) {
+            if (editRequestDto.getGender() != null ) {
                 user.setGender(editRequestDto.getGender());
             }
-            if (editRequestDto.getNickname() != null) {
+            if (editRequestDto.getNickname() != null && editRequestDto.getNickname()!="") {
                 user.setNickname(editRequestDto.getNickname());
             }
-            if (editRequestDto.getPassword() != null) {
+            if (editRequestDto.getPassword() != null && editRequestDto.getPassword()!="") {
                 account.setPassword(editRequestDto.getPassword());
             }
 
