7827ef6a8af5b5ecd6d5f5d6cb2eb3471345d323
Chen-Z-hub <2574680556@qq.com>
2025-04-24 16:47:02 +0800

    modified:   src/main/java/org/example/psychologicalcounseling/module/getAll/GetAllService.java

--- src/main/java/org/example/psychologicalcounseling/module/getAll/GetAllService.java
+++ src/main/java/org/example/psychologicalcounseling/module/getAll/GetAllService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.*;
 
 @Service
 public class GetAllService {
@@ -22,30 +23,64 @@ public class GetAllService {
         GetAllResponse getAllResponse = new GetAllResponse();
         List <GetAllResponse.Info> infoList = new ArrayList<>();
 
+
+
         if(role.equals("counsellor")){
+            // 使用线程池执行异步操作
+            ExecutorService executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+            List<CompletableFuture<Void>> futures = new ArrayList<>();
+
             // 假设从数据库中查询 counsellor 的数据
             List<Counsellor> counsellors = counsellorRepository.findAll();
+
             for (Counsellor counsellor : counsellors) {
-                infoList.add(
-                        new GetAllResponse.Info(
-                                counsellor.getCounsellorID(),
-                                counsellor.getNickname(),
-                                counsellor.getEmail()
-                        )
-                );
+                CompletableFuture<Void> future = CompletableFuture.runAsync(() -> {
+                    infoList.add(
+                            new GetAllResponse.Info(
+                                    counsellor.getCounsellorID(),
+                                    counsellor.getNickname(),
+                                    counsellor.getEmail()
+                            )
+                    );
+                }, executor);
+                futures.add(future);
             }
+
+            try {
+                CompletableFuture.allOf(futures.toArray(new CompletableFuture[0])).get();
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            } finally {
+                executor.shutdown();
+            }
+
         }else if (role.equals("supervisor")){
+            // 使用线程池执行异步操作
+            ExecutorService executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+            List<CompletableFuture<Void>> futures = new ArrayList<>();
+
             // 假设从数据库中查询 supervisor 的数据
             List<Supervisor> supervisors = supervisorRepository.findAll();
             for (Supervisor supervisor : supervisors) {
-                infoList.add(
-                        new GetAllResponse.Info(
-                                supervisor.getSupervisorID(),
-                                supervisor.getNickname(),
-                                supervisor.getEmail()
-                        )
-                );
+                CompletableFuture<Void> future = CompletableFuture.runAsync(() -> {
+                    infoList.add(
+                            new GetAllResponse.Info(
+                                    supervisor.getSupervisorID(),
+                                    supervisor.getNickname(),
+                                    supervisor.getEmail()
+                            )
+                    );
+                }, executor);
+                futures.add(future);
             }
+            try {
+                CompletableFuture.allOf(futures.toArray(new CompletableFuture[0])).get();
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }finally {
+                executor.shutdown();
+            }
+
         }else {
             getAllResponse.setCode(500);
             getAllResponse.setCodeMsg("Bad Request : Role Error");
