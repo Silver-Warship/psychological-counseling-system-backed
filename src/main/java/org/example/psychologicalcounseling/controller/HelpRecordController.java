a780445656d743ee8a2b036abed3b328e8b517dc
wubin <2538201831@qq.com>
2025-04-24 15:22:08 +0800

    feat 添加addHelpRecord接口

---
+++ src/main/java/org/example/psychologicalcounseling/controller/HelpRecordController.java
@@ -0,0 +1,50 @@
+package org.example.psychologicalcounseling.controller;
+
+
+import org.example.psychologicalcounseling.module.helpRecord.AddHelpRecordDto;
+import org.example.psychologicalcounseling.module.helpRecord.HelpRecordService;
+import org.example.psychologicalcounseling.repository.CounsellorRepository;
+import org.example.psychologicalcounseling.repository.HelpRecordRepository;
+import org.example.psychologicalcounseling.repository.SessionRepository;
+import org.example.psychologicalcounseling.repository.SupervisorRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelpRecordController {
+    private final CounsellorRepository counsellorRepository;
+    private final SupervisorRepository supervisorRepository;
+    private final SessionRepository sessionRepository;
+    private final HelpRecordService helpRecordService;
+
+    public HelpRecordController(CounsellorRepository counsellorRepository, SupervisorRepository supervisorRepository, SessionRepository sessionRepository, HelpRecordService helpRecordService) {
+        this.counsellorRepository = counsellorRepository;
+        this.supervisorRepository = supervisorRepository;
+        this.sessionRepository = sessionRepository;
+        this.helpRecordService = helpRecordService;
+    }
+
+    @PostMapping("/api/addHelpRecord")
+    ResponseEntity<?> addHelpRecord(@RequestParam AddHelpRecordDto dto) {
+        if (dto.getCounsellorID() == null || !counsellorRepository.existsById(dto.getCounsellorID())) {
+            return ResponseEntity.badRequest().body("Counsellor ID is invalid");
+        }
+
+        if (dto.getSupervisorID() == null || !supervisorRepository.existsById(dto.getSupervisorID())) {
+            return ResponseEntity.badRequest().body("Supervisor ID is invalid");
+        }
+
+        if (dto.getUserSessionID() == null || !sessionRepository.existsById(dto.getUserSessionID())) {
+            return ResponseEntity.badRequest().body("User session ID is invalid");
+        }
+
+        if (dto.getHelpSessionID() == null || !sessionRepository.existsById(dto.getHelpSessionID())) {
+            return ResponseEntity.badRequest().body("Help session ID is invalid");
+        }
+
+        helpRecordService.addHelpRecord(dto.getCounsellorID(), dto.getSupervisorID(), dto.getUserSessionID(), dto.getHelpSessionID());
+        return ResponseEntity.ok("Help record added successfully");
+    }
+}
