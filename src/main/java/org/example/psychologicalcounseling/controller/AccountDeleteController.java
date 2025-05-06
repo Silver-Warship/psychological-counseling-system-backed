301d01ed242b4c742ffd3e4960eed4a154b8eabd
Chen-Z-hub <2574680556@qq.com>
2025-05-06 23:26:36 +0800

    modified:   src/main/java/org/example/psychologicalcounseling/controller/AccountDeleteController.java 	modified:   src/main/java/org/example/psychologicalcounseling/module/info/AccountDeleteService.java

--- src/main/java/org/example/psychologicalcounseling/controller/AccountDeleteController.java
+++ src/main/java/org/example/psychologicalcounseling/controller/AccountDeleteController.java
@@ -1,7 +1,45 @@
 package org.example.psychologicalcounseling.controller;
 
 
-@
+import io.lettuce.core.dynamic.annotation.Param;
+import lombok.Data;
+import org.example.psychologicalcounseling.module.info.AccountDeleteService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.swing.text.html.parser.Entity;
+
+@RestController
+@RequestMapping("/api")
 public class AccountDeleteController {
 
+    final AccountDeleteService accountDeleteService;
+
+    public AccountDeleteController(AccountDeleteService accountDeleteService) {
+        this.accountDeleteService = accountDeleteService;
+    }
+
+    @PostMapping("/delete")
+    public ResponseEntity<?> deleteAccount(@Param("role") String role, @Param("uid") Long id) {
+
+        boolean result = false;
+
+        if (role.equals("user")) {
+            result = accountDeleteService.deleteUser(id);
+        } else if (role.equals("counsellor")) {
+            result = accountDeleteService.deleteCounsellor(id);
+        } else if (role.equals("supervisor")) {
+           result  = accountDeleteService.deleteSupervisor(id);
+        } else {
+            return ResponseEntity.badRequest().body("Invalidrole");
+        }
+
+        if(result){
+            return ResponseEntity.ok("Account deleted successfully");
+        }else {
+            return ResponseEntity.internalServerError().body("Failed to delete account");
+        }
+    }
 }
--- src/main/java/org/example/psychologicalcounseling/module/info/AccountDeleteService.java
+++ src/main/java/org/example/psychologicalcounseling/module/info/AccountDeleteService.java
@@ -1,4 +1,62 @@
 package org.example.psychologicalcounseling.module.info;
 
+
+import org.example.psychologicalcounseling.model.Account;
+import org.example.psychologicalcounseling.model.Counsellor;
+import org.example.psychologicalcounseling.model.Supervisor;
+import org.example.psychologicalcounseling.model.User;
+import org.example.psychologicalcounseling.repository.AccountRepository;
+import org.example.psychologicalcounseling.repository.CounsellorRepository;
+import org.example.psychologicalcounseling.repository.SupervisorRepository;
+import org.example.psychologicalcounseling.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AccountDeleteService {
+    private final UserRepository userRepository;
+    private final CounsellorRepository counsellorRepository;
+    private final SupervisorRepository supervisorRepository;
+    private final AccountRepository accountRepository;
+
+    public AccountDeleteService(UserRepository userRepository, CounsellorRepository counsellorRepository, SupervisorRepository supervisorRepository, AccountRepository accountRepository) {
+        this.userRepository = userRepository;
+        this.counsellorRepository = counsellorRepository;
+        this.supervisorRepository = supervisorRepository;
+        this.accountRepository = accountRepository;
+    }
+
+    public boolean deleteUser(Long id) {
+
+        User user = userRepository.findById(id).orElse(null);
+        Account account = accountRepository.findById(id).orElse(null);
+        if (user != null && account != null) {
+            userRepository.delete(user);
+            accountRepository.delete(account);
+            return true;
+        }
+        return false;
+
+    }
+
+    public boolean deleteCounsellor(Long id) {
+        Counsellor counsellor = counsellorRepository.findById(id).orElse(null);
+        Account account = accountRepository.findById(id).orElse(null);
+        if (counsellor != null && account != null) {
+            counsellorRepository.delete(counsellor);
+            accountRepository.delete(account);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean deleteSupervisor(Long id) {
+        Supervisor supervisor = supervisorRepository.findById(id).orElse(null);
+        Account account = accountRepository.findById(id).orElse(null);
+        if (supervisor != null && account != null) {
+            supervisorRepository.delete(supervisor);
+            accountRepository.delete(account);
+            return true;
+        }
+        return false;
+    }
 }
